fa5f9f45dda2dbde3052783e5c09d7dba9887e4d
Carson Bennett <carben2004@gmail.com>
2025-07-01 12:36:03 -0600

    Working on player X flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     private Vector2 movement;
+    private bool isFacingRight = true;
 
     void Update()
     {
@@ -15,5 +17,19 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position +  movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        if (movement.x > 0 && !isFacingRight)
+        {
+            isFacingRight = true;
+            spriteRenderer.flipX = true;
+        }
+        else if (movement.x < 0 && isFacingRight)
+        {
+            isFacingRight = false;
+            spriteRenderer.flipX = false;
+        }
     }
 }
